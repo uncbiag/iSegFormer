977bebc2f867db03fb8cad047027dd8921277fb1
Qin Liu <qinliu2020@gmail.com>
2023-02-23 10:53:42 -0500

    Propagate using the middle frame of the organ, not the middle frame of the volume.

--- maskprop/Med-STCN/abdomen1k-evaluation/evaluation_method.py
+++ maskprop/Med-STCN/abdomen1k-evaluation/evaluation_method.py
@@ -7,14 +7,7 @@ import pandas as pd
 
 
 
-def evaluate():
-    # Only two labels (label 5 and 6) will be evaluated
-    root_folder = '/playpen-raid2/qinliu'
-    gt_folder = f'{root_folder}/data/AbdomenCT-1K/Organ-12-Subset_frames/trainval/Annotations/480p'
-    # seg_folder = f'{root_folder}/projects/iSegFormer/maskprop/Med-STCN/results/stcn'
-    # seg_folder = f'{root_folder}/projects/iSegFormer/maskprop/Med-STCN/results/stcn_med_abdomen1k_Aug01_22.03.33_ft_s012_10k_cc'
-    seg_folder = f'{root_folder}/projects/iSegFormer/maskprop/Med-STCN/results/stcn_med_abdomen1k_Aug01_15.34.08_ft_s012_10k_no_cc'
-    dataset_file = f'{root_folder}/data/AbdomenCT-1K/Organ-12-Subset_frames/trainval/ImageSets/val.txt'
+def evaluate(gt_folder, seg_folder, dataset_file, label):
 
     videos = []
     with open(path.join(dataset_file), "r") as lines:
@@ -22,21 +15,15 @@ def evaluate():
             video = line.rstrip('\n')
             videos.append(video)
 
-    metrics_res_label_5 = {}
-    metrics_res_label_5['J'] = {"M": [], "R": [], "D": [], "M_per_object": {}}
-    metrics_res_label_5['F'] = {"M": [], "R": [], "D": [], "M_per_object": {}}
-
-    metrics_res_label_6 = {}
-    metrics_res_label_6['J'] = {"M": [], "R": [], "D": [], "M_per_object": {}}
-    metrics_res_label_6['F'] = {"M": [], "R": [], "D": [], "M_per_object": {}}
+    metrics_res_label = {}
+    metrics_res_label['J'] = {"M": [], "R": [], "D": [], "M_per_object": {}}
+    metrics_res_label['F'] = {"M": [], "R": [], "D": [], "M_per_object": {}}
 
     for video in videos:
         print('Evaluating ', video)
         frames = os.listdir(os.path.join(gt_folder, video))
-        iou_single_video_label_5 = []
-        iou_single_video_label_6 = []
-        bou_single_video_label_5 = []
-        bou_single_video_label_6 = []
+        iou_single_video_label = []
+        bou_single_video_label = []
         for idx, frame in enumerate(frames):
             gt_frame_file = os.path.join(gt_folder, video, '{:07d}.png'.format(idx))
             gt_frame = np.array(Image.open(gt_frame_file).convert('P'), dtype=np.uint8)
@@ -45,108 +32,77 @@ def evaluate():
             seg_frame = np.array(Image.open(seg_frame_file).convert('P'), dtype=np.uint8)
 
             # calculate J, M, and DSC across frames
-            gt_frame_label_5 = np.zeros_like(gt_frame)
-            gt_frame_label_5[gt_frame == 5] = 1
-            if np.count_nonzero(gt_frame_label_5) > 10:
-                seg_frame_label_5 = np.zeros_like(seg_frame)
-                seg_frame_label_5[seg_frame == 5] = 1
-                iou_label_5 = db_eval_iou(gt_frame_label_5, seg_frame_label_5)
-                iou_single_video_label_5.append(iou_label_5)
+            gt_frame_label = np.zeros_like(gt_frame)
+            gt_frame_label[gt_frame == label] = 1
+            if np.count_nonzero(gt_frame_label) > 10:
+                seg_frame_label = np.zeros_like(seg_frame)
+                seg_frame_label[seg_frame == label] = 1
+                iou_label = db_eval_iou(gt_frame_label, seg_frame_label)
+                iou_single_video_label.append(iou_label)
 
-                boundary_label_5 = db_eval_boundary(gt_frame_label_5, seg_frame_label_5)
-                bou_single_video_label_5.append(boundary_label_5)            
+                boundary_label = db_eval_boundary(gt_frame_label, seg_frame_label)
+                bou_single_video_label.append(boundary_label)
 
-            gt_frame_label_6 = np.zeros_like(gt_frame)
-            gt_frame_label_6[gt_frame == 6] = 1
-            if np.count_nonzero(gt_frame_label_6) > 10:
-                seg_frame_label_6 = np.zeros_like(seg_frame)
-                seg_frame_label_6[seg_frame == 6] = 1
-                iou_label_6 = db_eval_iou(gt_frame_label_6, seg_frame_label_6)
-                iou_single_video_label_6.append(iou_label_6)            
+        if len(iou_single_video_label) > 0:
+            [JM, JR, JD] = db_statistics(np.array(iou_single_video_label))
+            metrics_res_label['J']['M'].append(JM)
+            metrics_res_label['J']['R'].append(JR)
+            metrics_res_label['J']['D'].append(JD)
+            metrics_res_label['J']['M_per_object'][video] = JM        
 
-                boundary_label_6 = db_eval_boundary(gt_frame_label_6, seg_frame_label_6)
-                bou_single_video_label_6.append(boundary_label_6)
-
-        if len(iou_single_video_label_5) > 0:
-            [JM, JR, JD] = db_statistics(np.array(iou_single_video_label_5))
-            metrics_res_label_5['J']['M'].append(JM)
-            metrics_res_label_5['J']['R'].append(JR)
-            metrics_res_label_5['J']['D'].append(JD)
-            metrics_res_label_5['J']['M_per_object'][video] = JM
-
-        if len(iou_single_video_label_6) > 0:
-            [JM, JR, JD] = db_statistics(np.array(iou_single_video_label_6))
-            metrics_res_label_6['J']['M'].append(JM)
-            metrics_res_label_6['J']['R'].append(JR)
-            metrics_res_label_6['J']['D'].append(JD)
-            metrics_res_label_6['J']['M_per_object'][video] = JM        
-
-        if len(bou_single_video_label_5) > 0:
-            [FM, FR, FD] = db_statistics(np.array(bou_single_video_label_5))
-            metrics_res_label_5['F']['M'].append(FM)
-            metrics_res_label_5['F']['R'].append(FR)
-            metrics_res_label_5['F']['D'].append(FD)
-            metrics_res_label_5['F']['M_per_object'][video] = FM
-
-        if len(bou_single_video_label_6) > 0:
-            [FM, FR, FD] = db_statistics(np.array(bou_single_video_label_6))
-            metrics_res_label_6['F']['M'].append(FM)
-            metrics_res_label_6['F']['R'].append(FR)
-            metrics_res_label_6['F']['D'].append(FD)
-            metrics_res_label_6['F']['M_per_object'][video] = FM
+        if len(bou_single_video_label) > 0:
+            [FM, FR, FD] = db_statistics(np.array(bou_single_video_label))
+            metrics_res_label['F']['M'].append(FM)
+            metrics_res_label['F']['R'].append(FR)
+            metrics_res_label['F']['D'].append(FD)
+            metrics_res_label['F']['M_per_object'][video] = FM
 
     # Generate dataframe for the general results
     g_measures = ['J&F-Mean', 'J-Mean', 'J-Recall', 'J-Decay', 'F-Mean', 'F-Recall', 'F-Decay']
 
-    # results for label 5
-    label_5_J = metrics_res_label_5['J']
-    label_5_F = metrics_res_label_5['F']
-    final_mean_label_5 = (np.mean(label_5_J["M"]) + np.mean(label_5_F["M"])) / 2.
-    g_res_label_5 = np.array([final_mean_label_5, np.mean(label_5_J["M"]), np.mean(label_5_J["R"]), np.mean(label_5_J["D"]), \
-        np.mean(label_5_F["M"]), np.mean(label_5_F["R"]), np.mean(label_5_F["D"])])
-    g_res_label_5 = np.reshape(g_res_label_5, [1, len(g_res_label_5)])
-    table_g_label_5 = pd.DataFrame(data=g_res_label_5, columns=g_measures)
-
-    csv_path_label_5 = os.path.join(seg_folder, 'label_5.csv')
-    with open(csv_path_label_5, 'w') as f:
-        table_g_label_5.to_csv(f, index=False, float_format="%.3f")
-    print(f'Global results saved in {csv_path_label_5}')
-
     # results for label 6
-    label_6_J = metrics_res_label_6['J']
-    label_6_F = metrics_res_label_6['F']
-    final_mean_label_6 = (np.mean(label_6_J["M"]) + np.mean(label_6_F["M"])) / 2.
-    g_res_label_6 = np.array([final_mean_label_6, np.mean(label_6_J["M"]), np.mean(label_6_J["R"]), np.mean(label_6_J["D"]), \
-        np.mean(label_6_F["M"]), np.mean(label_6_F["R"]), np.mean(label_6_F["D"])])
-    g_res_label_6 = np.reshape(g_res_label_6, [1, len(g_res_label_6)])
-    table_g_label_6 = pd.DataFrame(data=g_res_label_6, columns=g_measures)
+    label_J = metrics_res_label['J']
+    label_F = metrics_res_label['F']
+    final_mean_label = (np.mean(label_J["M"]) + np.mean(label_F["M"])) / 2.
+    g_res_label = np.array([final_mean_label, np.mean(label_J["M"]), np.mean(label_J["R"]), np.mean(label_J["D"]), \
+        np.mean(label_F["M"]), np.mean(label_F["R"]), np.mean(label_F["D"])])
+    g_res_label = np.reshape(g_res_label, [1, len(g_res_label)])
+    table_g_label = pd.DataFrame(data=g_res_label, columns=g_measures)
 
-    csv_path_label_6 = os.path.join(seg_folder, 'label_6.csv')
-    with open(csv_path_label_6, 'w') as f:
-        table_g_label_6.to_csv(f, index=False, float_format="%.3f")
-    print(f'Global results saved in {csv_path_label_6}')
+    csv_path_label = os.path.join(seg_folder, f'label_{label}.csv')
+    with open(csv_path_label, 'w') as f:
+        table_g_label.to_csv(f, index=False, float_format="%.3f")
+    print(f'Global results saved in {csv_path_label}')
 
     # Generate a dataframe for the per sequence results
     seq_measures = ['Sequence', 'J-Mean', 'F-Mean']
 
-    csv_path_per_sequence_label_5 = os.path.join(seg_folder, 'label_5_per.csv')
-    seq_names_label_5 = list(label_5_J['M_per_object'].keys())
-    J_per_object_label_5 = [label_5_J['M_per_object'][x] for x in seq_names_label_5]
-    F_per_object_label_5 = [label_5_F['M_per_object'][x] for x in seq_names_label_5]
-    table_seq_label_5 = pd.DataFrame(data=list(zip(seq_names_label_5, J_per_object_label_5, F_per_object_label_5)), columns=seq_measures)
-    with open(csv_path_per_sequence_label_5, 'w') as f:
-        table_seq_label_5.to_csv(f, index=False, float_format="%.3f")
-    print(f'Per-sequence results saved in {csv_path_per_sequence_label_5}')
-
-    csv_path_per_sequence_label_6 = os.path.join(seg_folder, 'label_6_per.csv')
-    seq_names_label_6 = list(label_6_J['M_per_object'].keys())
-    J_per_object_label_6 = [label_6_J['M_per_object'][x] for x in seq_names_label_6]
-    F_per_object_label_6 = [label_6_F['M_per_object'][x] for x in seq_names_label_6]
-    table_seq_label_6 = pd.DataFrame(data=list(zip(seq_names_label_6, J_per_object_label_6, F_per_object_label_6)), columns=seq_measures)
-    with open(csv_path_per_sequence_label_6, 'w') as f:
-        table_seq_label_6.to_csv(f, index=False, float_format="%.3f")
-    print(f'Per-sequence results saved in {csv_path_per_sequence_label_6}')
+    csv_path_per_sequence_label = os.path.join(seg_folder, f'label_{label}_per.csv')
+    seq_names_label = list(label_J['M_per_object'].keys())
+    J_per_object_label = [label_J['M_per_object'][x] for x in seq_names_label]
+    F_per_object_label = [label_F['M_per_object'][x] for x in seq_names_label]
+    table_seq_label = pd.DataFrame(data=list(zip(seq_names_label, J_per_object_label, F_per_object_label)), columns=seq_measures)
+    with open(csv_path_per_sequence_label, 'w') as f:
+        table_seq_label.to_csv(f, index=False, float_format="%.3f")
+    print(f'Per-sequence results saved in {csv_path_per_sequence_label}')
 
 
 if __name__ == '__main__':
-    evaluate()
+
+    data_root_folder = '/playpen-raid2/qinliu/data/AbdomenCT-1K/Organ-12-Subset_frames/trainval'
+    gt_folder = f'{data_root_folder}/Annotations/480p'
+    dataset_file = f'{data_root_folder}/ImageSets/val.txt'
+
+    seg_root_folder = '/playpen-raid2/qinliu/projects/iSegFormer/maskprop/Med-STCN/results'
+    for label in range(1, 13):
+        seg_folder = f'{seg_root_folder}/stcn/label_{label}'
+        evaluate(gt_folder, seg_folder, dataset_file, label)
+
+    for label in range(1, 13):
+        seg_folder = f'{seg_root_folder}/stcn_med_abdomen1k_Aug01_15.34.08_ft_s012_10k_no_cc/label_{label}'
+        evaluate(gt_folder, seg_folder, dataset_file, label)
+
+    for label in range(1, 13):
+        seg_folder = f'{seg_root_folder}/stcn_med_abdomen1k_Aug01_22.03.33_ft_s012_10k_cc/label_{label}'
+        evaluate(gt_folder, seg_folder, dataset_file, label)
+
--- maskprop/Med-STCN/eval_med.py
+++ maskprop/Med-STCN/eval_med.py
@@ -40,85 +40,100 @@ palette = Image.open(path.expanduser(f'{med_path}/trainval/Annotations/480p/Orga
 
 torch.autograd.set_grad_enabled(False)
 
-# Setup Dataset
-valid_obj_labels = (5,6)
-if args.split == 'val':
-    test_dataset = MedTestDataset(med_path+'/trainval', imset='val.txt', valid_obj_labels=valid_obj_labels)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
-else:
-    raise NotImplementedError
+def evaluate_single_label(label):
+    valid_obj_labels = (label,)
 
-# Load our checkpoint
-top_k = args.top
-prop_model = STCN().cuda().eval()
+    # Setup Dataset
+    if args.split == 'val':
+        test_dataset = MedTestDataset(med_path+'/trainval', imset='val.txt', valid_obj_labels=valid_obj_labels)
+        test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
+    else:
+        raise NotImplementedError
 
-# Performs input mapping such that stage 0 model can be loaded
-prop_saved = torch.load(args.model)
-for k in list(prop_saved.keys()):
-    if k == 'value_encoder.conv1.weight':
-        if prop_saved[k].shape[1] == 4:
-            pads = torch.zeros((64,1,7,7), device=prop_saved[k].device)
-            prop_saved[k] = torch.cat([prop_saved[k], pads], 1)
-prop_model.load_state_dict(prop_saved)
+    # Load our checkpoint
+    top_k = args.top
+    prop_model = STCN().cuda().eval()
 
-total_process_time = 0
-total_frames = 0
+    # Performs input mapping such that stage 0 model can be loaded
+    prop_saved = torch.load(args.model)
+    for k in list(prop_saved.keys()):
+        if k == 'value_encoder.conv1.weight':
+            if prop_saved[k].shape[1] == 4:
+                pads = torch.zeros((64,1,7,7), device=prop_saved[k].device)
+                prop_saved[k] = torch.cat([prop_saved[k], pads], 1)
+    prop_model.load_state_dict(prop_saved)
 
-# start evaluation. Only 1 round propagation.
-for data in progressbar(test_loader, max_value=len(test_loader), redirect_stdout=True):
+    total_process_time = 0
+    total_frames = 0
 
-    with torch.cuda.amp.autocast(enabled=args.amp):
-        # example shape
-        # rgb: [1, T, 3, 480, 480]; msk: [N, T, 1, 480, 480]
-        # T is the number of frames; N is the number of organs        
-        rgb = data['rgb'].cuda()
-        msk = data['gt'][0].cuda()
-        info = data['info']
-        name = info['name'][0]
-        k = len(info['labels'][0])
-        size = info['size_480p']
-        num_frames = info['num_frames'].numpy()[0]
+    # start evaluation. Only 1 round propagation.
+    for data in progressbar(test_loader, max_value=len(test_loader), redirect_stdout=True):
 
-        start_frame = num_frames // 2
-        # find the best starting frame
-        
-        torch.cuda.synchronize()
-        process_begin = time.time()
+        with torch.cuda.amp.autocast(enabled=args.amp):
+            # example shape
+            # rgb: [1, T, 3, 480, 480]; msk: [N, T, 1, 480, 480]
+            # T is the number of frames; N is the number of organs        
+            rgb = data['rgb'].cuda()
+            msk = data['gt'][0].cuda()
+            info = data['info']
+            name = info['name'][0]
+            k = len(info['labels'][0])
+            size = info['size_480p']
+            num_frames = info['num_frames'].numpy()[0]
 
-        processor = InferenceCore(prop_model, rgb, k, top_k=top_k, 
-                        mem_every=args.mem_every, include_last=args.include_last)
-        processor.interact(msk[:,start_frame], start_frame)
+            # find the best starting frame
+            max_area, max_area_idx = -1, num_frames // 2
+            for i in range(msk.shape[1]):
+                area = torch.count_nonzero(msk[:,i])
+                if area > max_area:
+                    max_area = area
+                    max_area_idx = i
 
-        # Do unpad -> upsample to original size 
-        out_masks = torch.zeros((processor.t, 1, *size), dtype=torch.uint8, device='cuda')
-        for ti in range(processor.t):
-            prob = unpad(processor.prob[:,ti], processor.pad)
-            prob = F.interpolate(prob, size, mode='bilinear', align_corners=False)
-            out_masks[ti] = torch.argmax(prob, dim=0)
+            torch.cuda.synchronize()
+            process_begin = time.time()
 
-        out_masks = (out_masks.detach().cpu().numpy()[:,0]).astype(np.uint8)
+            processor = InferenceCore(prop_model, rgb, k, top_k=top_k, 
+                            mem_every=args.mem_every, include_last=args.include_last)
+            processor.interact(msk[:,max_area_idx], max_area_idx)
 
-        # cautious: no overlap between the two label sets
-        # needs a label mapper and re-allocate the memory
-        for idx, label in enumerate(valid_obj_labels):
-            out_masks[out_masks == idx + 1] = label
+            # Do unpad -> upsample to original size 
+            out_masks = torch.zeros((processor.t, 1, *size), dtype=torch.uint8, device='cuda')
+            for ti in range(processor.t):
+                prob = unpad(processor.prob[:,ti], processor.pad)
+                prob = F.interpolate(prob, size, mode='bilinear', align_corners=False)
+                out_masks[ti] = torch.argmax(prob, dim=0)
 
-        torch.cuda.synchronize()
-        total_process_time += time.time() - process_begin
-        total_frames += out_masks.shape[0]
+            out_masks = (out_masks.detach().cpu().numpy()[:,0]).astype(np.uint8)
 
-        # Save the results
-        this_out_path = path.join(out_path, name)
-        os.makedirs(this_out_path, exist_ok=True)
-        for f in range(out_masks.shape[0]):
-            img_E = Image.fromarray(out_masks[f])
-            img_E.putpalette(palette)
-            img_E.save(os.path.join(this_out_path, '{:05d}.png'.format(f)))
+            # cautious: no overlap between the two label sets
+            # needs a label mapper and re-allocate the memory
+            for idx, label in enumerate(valid_obj_labels):
+                out_masks[out_masks == idx + 1] = label
 
-        del rgb
-        del msk
-        del processor
+            torch.cuda.synchronize()
+            total_process_time += time.time() - process_begin
+            total_frames += out_masks.shape[0]
 
-print('Total processing time: ', total_process_time)
-print('Total processed frames: ', total_frames)
-print('FPS: ', total_frames / total_process_time)
+            # Save the results
+            this_out_path = path.join(out_path, 'label_'+'_'.join([str(i) for i in valid_obj_labels]), name)
+            os.makedirs(this_out_path, exist_ok=True)
+            for f in range(out_masks.shape[0]):
+                img_E = Image.fromarray(out_masks[f])
+                img_E.putpalette(palette)
+                img_E.save(os.path.join(this_out_path, '{:05d}.png'.format(f)))
+
+            del rgb
+            del msk
+            del processor
+
+    print('Total processing time: ', total_process_time)
+    print('Total processed frames: ', total_frames)
+    print('FPS: ', total_frames / total_process_time)
+
+
+
+if __name__ == '__main__':
+
+    for label in range(1, 13):
+        print(f'Processing label {label} ...')
+        evaluate_single_label(label)
