4d8f4d8bb3f19e2e4b4023d34f14b0b03394bbeb
Qin Liu <qinliu2020@gmail.com>
2023-02-25 19:45:33 -0500

    Add pipleline for training with fusion loss

--- maskprop/Med-STCN/run_train_s4_cc.sh
+++ maskprop/Med-STCN/run_train_s4_cc.sh
@@ -1,7 +1,7 @@
-git config --global --add safe.directory /work/projects/STCN
+git config --global --add safe.directory /playpen-raid2/qinliu/models/model_0224_2023
 
 CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=2 python3 -m torch.distributed.launch \
 --master_port 9843 --nproc_per_node=2 train.py --id retrain_s4_ft_from_med \
---load_network /work/projects/STCN/saves/stcn.pth \
+--load_network /playpen-raid2/qinliu/projects/iSegFormer/maskprop/Med-STCN/saves/stcn.pth \
 --stage 4 \
 --use_cycle_loss
