e1f3dfa3667df63943503e6225451c9bac7f57ac
Qin Liu <qinliu2020@gmail.com>
2023-03-02 17:18:57 -0500

    Update the demo script

--- maskprop/MiVOS/run_demo.sh
+++ maskprop/MiVOS/run_demo.sh
@@ -1,1 +1,4 @@
-python3 interactive_gui.py --images /playpen-raid2/qinliu/projects/iSegFormer/maskprop/XMem/workspace/lung_001/images/ --num_objects=4
+
+volume_1=/playpen-raid2/qinliu/projects/iSegFormer/maskprop/XMem/workspace/lung_001/images/
+volume_2=/playpen-raid2/qinliu/data/AbdomenCT-1K/Organ-12-Subset_frames/trainval/JPEGImages/480p/Organ12_0001
+python3 interactive_gui.py --images ${volume_2} --num_objects=4
