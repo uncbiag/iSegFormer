0bbb20742759f8e09db7b21b7d53bd9e5b353140
Qin Liu <qinliu2020@gmail.com>
2024-03-09 15:36:21 -0500

    Refactor a little bit

---
+++ run_train_stcn_without_cycle.sh
@@ -0,0 +1,10 @@
+# finetune a video-pretrained STCN model on medical images for 10k iterations
+# without cycle consistency loss
+
+torchrun ./maskprop/Med-STCN/train.py \
+--id retrain_s4_ft_from_med \
+--load_network ./saves/stcn.pth \
+--stage 4 \
+--batch_size 10 \
+--iterations 10000 \
+--save_model_interval 10000 
